14deb3a7074e903cf2d0c4faa0497091e621fc30
Muni <pallamuniswamy@gmail.com>
2018-04-30 04:32:24 +0530

    kfjlsdfjk

--- limits-service/src/main/java/com/spring/microservices/LimitsController.java
+++ limits-service/src/main/java/com/spring/microservices/LimitsController.java
@@ -4,14 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitsController {
-	
+
 	@Autowired
 	private LimitsConfiguration limitsConfig;
 
 	@GetMapping("/limits")
+	@HystrixCommand(fallbackMethod = "fault-tolerance")
 	public LimitsConfiguration retriveConfigFromCloud() {
 		return new LimitsConfiguration(limitsConfig.getMin(), limitsConfig.getMax());
+	}
+
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+	public LimitsConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+
+	public LimitsConfiguration fallbackRetrieveConfiguration() {
+		return new LimitsConfiguration(999, 9);
 	}
 }
