d9bd702a56c73519035d1150209aabb111f6e6ed
Muni <pallamuniswamy@gmail.com>
2018-05-01 15:35:07 +0530

    jhdslkfj

--- currency-conversion-service/src/main/java/com/spring/microservices/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/spring/microservices/CurrencyExchangeServiceProxy.java
@@ -5,13 +5,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient("currency-exchange-service")
+//@FeignClient("currency-exchange-service")//with out zuul
 @FeignClient("zuul-api-gateway-server")
-@RibbonClient("currency-exchange-service")
+@RibbonClient("currency-exchange-service")//if your request want go through the zuul api gareway
 public interface CurrencyExchangeServiceProxy {
 
-	// @GetMapping("/currency-exchange/from/{from}/to/{to}")
-	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+	// @GetMapping("/currency-exchange/from/{from}/to/{to}")//with out zuul
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")//if your request want go through the zuul api gareway
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from,
 			@PathVariable("to") String to);
 
